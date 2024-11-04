f98db1d1ce5943ec663141bd6c66154f4a328eb5
LLL707 <24s601h0659@sonline20.sen.go.kr>
2024-11-04 20:23:48 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript 코드
+window.onload = function() {
+    const heading = document.querySelector('h3');
+    heading.onclick = function() {
+        alert('인공지능소프트웨어과에 오신 것을 환영합니다!');
+    };
+};
